e984e2a5f31c3eda91da15f6886570dab6a99711
take_cheeze <takechi101010@gmail.com>
2015-06-10 10:50:30 +0900

    Use mruby clang plugin on travis build.

--- .travis_config.rb
+++ .travis_config.rb
@@ -1,7 +1,12 @@
 MRuby::Build.new do |conf|
-  toolchain :gcc
+  toolchain :clang
+
   enable_bintest
   enable_debug
+
+  plugin_flags = %W[-Xclang -load -Xclang "#{MRUBY_ROOT}/../libmruby-clang-checker.so" -Xclang -add-plugin -Xclang mruby-clang-checker]
+  cc.flags += plugin_flags
+  cxx.flags += plugin_flags
 
   conf.gem '.'
 end
